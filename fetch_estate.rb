7677f7fa687cd830e157d79e1cfff6acb0bca61f
Makoto Onami <makoto.onami@livesense.co.jp>
2018-01-29 18:47:04 +0900

    added patterns

---
+++ fetch_estate.rb
@@ -0,0 +1,51 @@
+require 'faraday'
+require 'json'
+
+API_KEY=ENV['RESAS_API_KEY']
+host = 'https://opendata.resas-portal.go.jp'
+endpoint = '/api/v1/townPlanning/estateTransaction/bar'
+
+tokyo_23regions = {
+  13101 => '千代田区',
+  13102 => '中央区',
+  13103 => '港区',
+  13104 => '新宿区',
+  13105 => '文京区',
+  13106 => '台東区',
+  13107 => '墨田区',
+  13108 => '江東区',
+  13109 => '品川区',
+  13110 => '目黒区',
+  13111 => '大田区',
+  13112 => '世田谷区',
+  13113 => '渋谷区',
+  13114 => '中野区',
+  13115 => '杉並区',
+  13116 => '豊島区',
+  13117 => '北区',
+  13118 => '荒川区',
+  13119 => '板橋区',
+  13120 => '練馬区',
+  13121 => '足立区',
+  13122 => '葛飾区',
+  13123 => '江戸川区'
+}
+
+response_group = tokyo_23regions.each_with_object({}) do |(cityCode, cityName), h|
+  h[cityName] = {}
+  2009.upto(2015) do |year|
+    response = Faraday.new.get do |req|
+      req.url "#{host}#{endpoint}?prefCode=13&displayType=2&year=#{year}&cityCode=#{cityCode}"
+      req.headers['X-API-KEY'] = API_KEY
+    end
+
+    amounts = JSON.parse(response.body)['result']
+    h[cityName][year] = amounts['years'][0]['value']
+  end
+end
+
+p ['区名', *response_group.first[1].keys].join(',')
+
+response_group.each do |cityName, amounts|
+  p [cityName, *amounts.values].join(',')
+end
